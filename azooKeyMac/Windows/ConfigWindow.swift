6d81cf720c0b4617841d6fc857fcc52bcc5d7142
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-05-10 11:08:42 +0900

    feat: full width configure view

--- azooKeyMac/Windows/ConfigWindow.swift
+++ azooKeyMac/Windows/ConfigWindow.swift
@@ -44,95 +44,92 @@ struct ConfigWindow: View {
             Spacer()
             HStack {
                 Spacer()
-                ScrollView {
-                    Form {
+                Form {
 
-                        Picker("履歴学習", selection: $learning) {
-                            Text("学習する").tag(Config.Learning.Value.inputAndOutput)
-                            Text("学習を停止").tag(Config.Learning.Value.onlyOutput)
-                            Text("学習を無視").tag(Config.Learning.Value.nothing)
-                        }
-                        Picker("パーソナライズ", selection: $zenzaiPersonalizationLevel) {
-                            Text("オフ").tag(Config.ZenzaiPersonalizationLevel.Value.off)
-                            Text("弱く").tag(Config.ZenzaiPersonalizationLevel.Value.soft)
-                            Text("普通").tag(Config.ZenzaiPersonalizationLevel.Value.normal)
-                            Text("強く").tag(Config.ZenzaiPersonalizationLevel.Value.hard)
-                        }
-                        Divider()
-                        HStack {
-                            Toggle("Zenzaiを有効化", isOn: $zenzai)
-                            helpButton(helpContent: "Zenzaiはニューラル言語モデルを利用した最新のかな漢字変換システムです。\nMacのGPUを利用して高精度な変換を行います。\n変換エンジンはローカルで動作するため、外部との通信は不要です。", isPresented: $zenzaiHelpPopover)
-                        }
-                        HStack {
-                            TextField("変換プロフィール", text: $zenzaiProfile, prompt: Text("例：田中太郎/高校生"))
-                                .disabled(!zenzai.value)
-                            helpButton(
-                                helpContent: """
-                            Zenzaiはあなたのプロフィールを考慮した変換を行うことができます。
-                            名前や仕事、趣味などを入力すると、それに合わせた変換が自動で推薦されます。
-                            （実験的な機能のため、精度が不十分な場合があります）
-                            """,
-                                isPresented: $zenzaiProfileHelpPopover
-                            )
-                        }
-                        HStack {
-                            TextField(
-                                "Zenzaiの推論上限",
-                                text: Binding(
-                                    get: {
-                                        String(self.$inferenceLimit.wrappedValue)
-                                    },
-                                    set: {
-                                        if let value = Int($0), (1 ... 50).contains(value) {
-                                            self.$inferenceLimit.wrappedValue = value
-                                        }
-                                    }
-                                )
-                            )
-                            .disabled(!zenzai.value)
-                            Stepper("", value: $inferenceLimit, in: 1 ... 50)
-                                .labelsHidden()
-                                .disabled(!zenzai.value)
-                            helpButton(helpContent: "推論上限を小さくすると、入力中のもたつきが改善されることがあります。", isPresented: $zenzaiInferenceLimitHelpPopover)
-                        }
-                        Divider()
-                        Toggle("ライブ変換を有効化", isOn: $liveConversion)
-                        Toggle("円記号の代わりにバックスラッシュを入力", isOn: $typeBackSlash)
-                        Toggle("「、」「。」の代わりに「，」「．」を入力", isOn: $typeCommaAndPeriod)
-                        Toggle("スペースは常に半角を入力", isOn: $typeHalfSpace)
-                        Divider()
-                        Button("ユーザ辞書を編集する") {
-                            (NSApplication.shared.delegate as? AppDelegate)!.openUserDictionaryEditorWindow()
-                        }
-                        Divider()
-                        Toggle("（開発者用）デバッグウィンドウを有効化", isOn: $debugWindow)
-                        Toggle("OpenAI APIキーの利用", isOn: $enableOpenAiApiKey)
-                        HStack {
-                            SecureField("OpenAI API", text: $openAiApiKey, prompt: Text("例:sk-xxxxxxxxxxx"))
-                            helpButton(
-                                helpContent: "OpenAI APIキーはローカルのみで管理され、外部に公開されることはありません。生成の際にAPIを利用するため、課金が発生します。",
-                                isPresented: $openAiApiKeyPopover
-                            )
-                        }
-                        TextField("OpenAI Model Name", text: $openAiModelName, prompt: Text("例: gpt-4o-mini"))
-                            .disabled(!$enableOpenAiApiKey.wrappedValue)
-                        LabeledContent("Version") {
-                            Text(PackageMetadata.gitTag ?? PackageMetadata.gitCommit ?? "Unknown Version")
-                                .monospaced()
-                                .bold()
-                                .copyable([
-                                    PackageMetadata.gitTag ?? PackageMetadata.gitCommit ?? "Unknown Version"
-                                ])
-                        }
-                        .textSelection(.enabled)
+                    Picker("履歴学習", selection: $learning) {
+                        Text("学習する").tag(Config.Learning.Value.inputAndOutput)
+                        Text("学習を停止").tag(Config.Learning.Value.onlyOutput)
+                        Text("学習を無視").tag(Config.Learning.Value.nothing)
                     }
+                    Picker("パーソナライズ", selection: $zenzaiPersonalizationLevel) {
+                        Text("オフ").tag(Config.ZenzaiPersonalizationLevel.Value.off)
+                        Text("弱く").tag(Config.ZenzaiPersonalizationLevel.Value.soft)
+                        Text("普通").tag(Config.ZenzaiPersonalizationLevel.Value.normal)
+                        Text("強く").tag(Config.ZenzaiPersonalizationLevel.Value.hard)
+                    }
+                    Divider()
+                    HStack {
+                        Toggle("Zenzaiを有効化", isOn: $zenzai)
+                        helpButton(helpContent: "Zenzaiはニューラル言語モデルを利用した最新のかな漢字変換システムです。\nMacのGPUを利用して高精度な変換を行います。\n変換エンジンはローカルで動作するため、外部との通信は不要です。", isPresented: $zenzaiHelpPopover)
+                    }
+                    HStack {
+                        TextField("変換プロフィール", text: $zenzaiProfile, prompt: Text("例：田中太郎/高校生"))
+                            .disabled(!zenzai.value)
+                        helpButton(
+                            helpContent: """
+                        Zenzaiはあなたのプロフィールを考慮した変換を行うことができます。
+                        名前や仕事、趣味などを入力すると、それに合わせた変換が自動で推薦されます。
+                        （実験的な機能のため、精度が不十分な場合があります）
+                        """,
+                            isPresented: $zenzaiProfileHelpPopover
+                        )
+                    }
+                    HStack {
+                        TextField(
+                            "Zenzaiの推論上限",
+                            text: Binding(
+                                get: {
+                                    String(self.$inferenceLimit.wrappedValue)
+                                },
+                                set: {
+                                    if let value = Int($0), (1 ... 50).contains(value) {
+                                        self.$inferenceLimit.wrappedValue = value
+                                    }
+                                }
+                            )
+                        )
+                        .disabled(!zenzai.value)
+                        Stepper("", value: $inferenceLimit, in: 1 ... 50)
+                            .labelsHidden()
+                            .disabled(!zenzai.value)
+                        helpButton(helpContent: "推論上限を小さくすると、入力中のもたつきが改善されることがあります。", isPresented: $zenzaiInferenceLimitHelpPopover)
+                    }
+                    Divider()
+                    Toggle("ライブ変換を有効化", isOn: $liveConversion)
+                    Toggle("円記号の代わりにバックスラッシュを入力", isOn: $typeBackSlash)
+                    Toggle("「、」「。」の代わりに「，」「．」を入力", isOn: $typeCommaAndPeriod)
+                    Toggle("スペースは常に半角を入力", isOn: $typeHalfSpace)
+                    Divider()
+                    Button("ユーザ辞書を編集する") {
+                        (NSApplication.shared.delegate as? AppDelegate)!.openUserDictionaryEditorWindow()
+                    }
+                    Divider()
+                    Toggle("（開発者用）デバッグウィンドウを有効化", isOn: $debugWindow)
+                    Toggle("OpenAI APIキーの利用", isOn: $enableOpenAiApiKey)
+                    HStack {
+                        SecureField("OpenAI API", text: $openAiApiKey, prompt: Text("例:sk-xxxxxxxxxxx"))
+                        helpButton(
+                            helpContent: "OpenAI APIキーはローカルのみで管理され、外部に公開されることはありません。生成の際にAPIを利用するため、課金が発生します。",
+                            isPresented: $openAiApiKeyPopover
+                        )
+                    }
+                    TextField("OpenAI Model Name", text: $openAiModelName, prompt: Text("例: gpt-4o-mini"))
+                        .disabled(!$enableOpenAiApiKey.wrappedValue)
+                    LabeledContent("Version") {
+                        Text(PackageMetadata.gitTag ?? PackageMetadata.gitCommit ?? "Unknown Version")
+                            .monospaced()
+                            .bold()
+                            .copyable([
+                                PackageMetadata.gitTag ?? PackageMetadata.gitCommit ?? "Unknown Version"
+                            ])
+                    }
+                    .textSelection(.enabled)
                 }
                 Spacer()
             }
             Spacer()
         }
-        .frame(minHeight: 300, maxHeight: 600)
-        .frame(minWidth: 400, maxWidth: 600)
+        .frame(minWidth: 500)
     }
 }
 
