62c714841bf330e4d244100d449e68ebfe55f70c
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2025-06-01 11:56:56 +0900

    feat: cache-safe version of git information

--- Core/Package.swift
+++ Core/Package.swift
@@ -3,18 +3,11 @@
 
 import PackageDescription
 
-let gitTag: String = "\"" + (Context.gitInformation?.currentTag ?? "unknown") + "\""
-let gitCommit: String = "\"" + (Context.gitInformation?.currentCommit ?? "unknown") + "\""
-
 let package = Package(
     name: "Core",
     platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Metadata",
-            targets: ["Metadata"]
-        ),
         .library(
             name: "Core",
             targets: ["Core"]
@@ -24,23 +17,22 @@ let package = Package(
         .package(url: "https://github.com/azooKey/AzooKeyKanaKanjiConverter", from: "0.8.0", traits: ["Zenzai"]),
     ],
     targets: [
-        .target(
-            name: "Metadata",
-            cSettings: [
-                .define(
-                    "GIT_TAG", to: gitTag
-                ),
-                .define(
-                    "GIT_COMMIT", to: gitCommit
-                ),
-            ]
+        .executableTarget(
+            name: "git-info-generator",
+        ),
+        .plugin(
+            name: "GitInfoPlugin",
+            capability: .buildTool(),
+            dependencies: [.target(name: "git-info-generator")]
         ),
         .target(
             name: "Core",
             dependencies: [
-                .target(name: "Metadata"),
                 .product(name: "SwiftUtils", package: "AzooKeyKanaKanjiConverter"),
                 .product(name: "KanaKanjiConverterModuleWithDefaultDictionary", package: "AzooKeyKanaKanjiConverter"),
+            ],
+            plugins: [
+                .plugin(name: "GitInfoPlugin")
             ],
         ),
         .testTarget(
